115d5569214670b789254b92b340742b3e0a1f36
Akshita-Lathar <al4848@columbia.edu>
2025-04-27 15:41:35 -0700

    UI Changes

--- static/overview.js
+++ static/overview.js
@@ -80,7 +80,7 @@ function showLabel(button, position) {
     button.setAttribute("data-clicked", "true");
 
     // Change the color of the clicked hotspot to a lighter shade
-    button.style.backgroundColor = "rgba(255, 0, 0, 0.25)"; // Lighten the red color
+    button.style.backgroundColor = "#a6c8e5"; // Lighten the red color
 
     if (!clickedMap[currentIndex]) {
         clickedMap[currentIndex] = new Set();

4c581dae22db4c99bc8411eaa6dd9f94210e6b32
panache <vaswani.akashj@gmail.com>
2025-04-28 00:39:52 -0400

    updated styling

--- static/quiz_results.js
+++ static/quiz_results.js
@@ -5,18 +5,28 @@ $(function () {
 
     // Populate the feedback
     const $feedbackContainer = $(".feedback-container");
+    let hasIncorrectAnswers = false; // Track if there are any incorrect answers
+
     feedback.forEach((questionFeedback, questionIndex) => {
-
         questionFeedback.forEach((answer) => {
-            const $answerDiv = $("<div></div>").addClass("answer-feedback");
-
             if (!answer.correct) {
+                hasIncorrectAnswers = true; // Mark that there is at least one incorrect answer
+
+                // Create a feedback box for the incorrect answer
+                const $answerDiv = $("<div></div>").addClass("answer-feedback");
+
+                // Add the explanation for the incorrect answer
                 const $explanation = $("<p></p>").html(answer.explanation);
                 $answerDiv.append($explanation);
-            }
-            $feedbackContainer.append($answerDiv);
-        });
 
-        
+                // Append the feedback box to the container
+                $feedbackContainer.append($answerDiv);
+            }
+        });
     });
+
+    // If all answers are correct, hide the feedback section
+    if (!hasIncorrectAnswers) {
+        $feedbackContainer.remove(); // Remove the feedback container
+    }
 });
